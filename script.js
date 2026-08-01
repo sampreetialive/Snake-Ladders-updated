80b53062050ac08ebeb4846c6baf816c2427d2a8
Sam G <sampreetighosh08@gmail.com>
2026-08-02 00:16:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const canvas = document.getElementById("board");
 
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillStyle = "#d8b36a";
+ctx.fillRect(0,0,800,800);
+
+ctx.fillStyle="black";
+ctx.font="40px Arial";
+ctx.fillText("Snake & Ladders Deluxe",120,400);
